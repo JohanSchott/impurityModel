ad18549a8d59862bbca7f1d97f79a063e42b0395
Johan Schott <johan.schott@gmail.com>
2021-07-26 20:50:54 +0200

    remove test from install-script. Also specify all python versions. And use pip-compile.

--- install_setup.sh
+++ install_setup.sh
@@ -6,13 +6,12 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 # Create virtual environment. But only if it does not already exist.
 test -d ~/envED || virtualenv -p python3.7 ~/envED
 
-# Activate virtual environment and append to PYTHONPATH.
-source env.sh
+# Activate virtual environment.
+source ~/envED/bin/activate
 
 # Install required python libraries.
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip==21.1.2
+python -m pip install pip-tools==6.1.0
+pip-compile
 pip install -r requirements.txt
-
-# Run unit-tests
-pytest
 
