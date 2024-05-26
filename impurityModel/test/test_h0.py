4a5f3425969f441c385d3749dd9b2db62e6dd276
Johan Schott <johan.schott@gmail.com>
2024-05-26 23:34:19 +0200

    ruff fix

--- impurityModel/test/test_h0.py
+++ impurityModel/test/test_h0.py
@@ -1,9 +1,8 @@
 import os
 from glob import glob
 
-from impurityModel.ed.get_spectra import read_pickled_file
 from impurityModel.ed.finite import check_hermitian
-
+from impurityModel.ed.get_spectra import read_pickled_file
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
